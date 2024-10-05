dfc05a50c25e018bf4513790c4556e34fd4f831e
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-10-05 23:17:51 +0200

    📦 NEW: Add player class and gravity

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+import { Player } from "./js/player.js";
+
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = "#A3CFA7";
-c.fillRect(0, 0, canvas.width, canvas.height);
+const player = new Player({ x: 0, y: 0 });
+const player2 = new Player({ x: 300, y: 100 });
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillStyle = "#A3CFA7";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    player2.update();
+}
+
+animate();
